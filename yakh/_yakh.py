ef01d540c3d40b9732cc7566d50a75e72b9d503e
petereon <pvyboch1@gmail.com>
2022-12-05 12:19:54 +0100

    docs: adding more docstrings

--- yakh/_yakh.py
+++ yakh/_yakh.py
@@ -6,7 +6,7 @@ try:
     import termios
     import tty
 
-    def __get_key() -> bytes:
+    def __get_key() -> str:
         fd_input = sys.stdin.fileno()
         term_attr = termios.tcgetattr(fd_input)
         tty.setraw(fd_input)
@@ -15,6 +15,11 @@ try:
         return ch_str
 
     def get_key() -> Key:
+        """Returns a `Key` instance representing a keypress
+
+        Returns:
+            Key: Represents a keypress
+        """
         ch_str = __get_key()
         ch_ord = tuple(map(ord, ch_str))
         return Key(ch_str, ch_ord, ch_str.isprintable() or ch_ord in [(13,), (27, 13)])
@@ -24,6 +29,11 @@ except ImportError:
     import msvcrt
 
     def get_key() -> Key:
+        """Returns a `Key` instance representing a keypress
+
+        Returns:
+            Key: Represents a keypress
+        """
         ch_str = msvcrt.getwch()
         if ch_str in ["à", "\x00"]:
             ch_str += msvcrt.getwch()
